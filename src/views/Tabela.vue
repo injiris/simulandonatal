f753ce84316fc3f1e9ce81b4d75170bb639d5a7b
Vinicius Matos <34461311+VinciusAndre@users.noreply.github.com>
2021-03-11 20:57:40 -0300

    Implementando o script

--- src/views/Tabela.vue
+++ src/views/Tabela.vue
@@ -3,7 +3,23 @@
 </template>
 <script>
 export default {
-  name:'Tabela'
+  name:'Tabela',
+  computed: {
+    ordernarClubes(){
+      const ordernar = this.clubesTabela.slice(0).sort((a,b)=> a.pontos > b.pontos ? -1 : 1 )
+      return ordernar;
+    }
+  },
+  data(){
+    return {
+      clubesTabela: []
+    }
+  },
+  created(){
+    fetch('https://hackthon-decola.firebaseio.com/clubes-lista.json')
+    .then(res => res.json()
+    .then(json => {this.clubesTabela = json}))
+  }
 }
 </script>
 <style scoped>
